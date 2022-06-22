fc5449ae2f16f987cb32795e2e1eabd9436e6a6d
NotTerrence <terencedeindugang@gmail.com>
2022-06-22 16:22:47 +0800

    added yo portfolio

---
+++ -outdated-Rico-Shot-master/Rico-Shot v2/Assets/Gameplay/Scripts/playerHit.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerHit : MonoBehaviour {
+
+    GameObject player;
+    public gameOver over;
+
+    // Use this for initialization
+    void OnCollisionEnter(Collision collision)
+    {
+        //Check for a match with the specific tag on any GameObject that collides with your GameObject
+        if (collision.gameObject.tag == "Ball")
+        {
+            //If the GameObject has the same tag as specified, output this message in the console
+            SumScore.SaveHighScore();
+            SumScore.Reset();
+            GameObject[] balls = GameObject.FindGameObjectsWithTag("Ball");
+
+            for (var i = 0; i < balls.Length; i++)
+                Destroy(balls[i]);
+
+            over.playerHit();
+        }
+    }
+}
